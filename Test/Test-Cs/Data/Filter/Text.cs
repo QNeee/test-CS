12a18caecab1b595e9889f56d4ad455c3a6e114d
QNeee <vovasagan7@gmail.com>
2026-03-27 14:14:00 +0200

    end

--- Test/Test-Cs/Data/Filter/Text.cs
+++ Test/Test-Cs/Data/Filter/Text.cs
@@ -2,14 +2,14 @@
 
 namespace Test_Cs.Data.Filter
 {
-    internal class Text : IFilter
+    internal class Text : Filter 
     {
-        public void Execute(Post item, string value, List<ResponseObj> list)
+        public override void Execute(Post item, string value, List<ResponseObj> list)
         {
             bool isMatch = item.text.Contains(value, StringComparison.OrdinalIgnoreCase);
-            if (isMatch)
+            if (isMatch && _addedTexts.Add(item.text))
             {
-                var obj = new ResponseObj(item.text, String.IsNullOrEmpty(item.url));
+                var obj = new ResponseObj(item.text, !String.IsNullOrEmpty(item.url));
                 list.Add(obj);
             }
         }
--- Test/Test-Cs/Data/Filter/Title.cs
+++ Test/Test-Cs/Data/Filter/Title.cs
@@ -2,14 +2,15 @@
 
 namespace Test_Cs.Data.Filter
 {
-    internal class Title : IFilter
+    internal class Title : Filter
     {
-        public void Execute(Post item, string value, List<ResponseObj> list)
+
+        public override void Execute(Post item, string value, List<ResponseObj> list)
         {
             bool isMatch = item.title.Contains(value, StringComparison.OrdinalIgnoreCase);
-            if (isMatch)
+            if (isMatch && _addedTexts.Add(item.title))
             {
-                var obj = new ResponseObj(item.title, String.IsNullOrEmpty(item.url));
+                var obj = new ResponseObj(item.title, !String.IsNullOrEmpty(item.url));
                 list.Add(obj);
             }
         }
